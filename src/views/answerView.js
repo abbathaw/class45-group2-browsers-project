058ccc0146e1d0e7c5057c6f7973b19c17eb44f7
Kumait <137179507+29Kumait@users.noreply.github.com>
2023-09-25 21:09:21 +0200

    add title

--- src/views/answerView.js
+++ src/views/answerView.js
@@ -7,5 +7,10 @@ export const createAnswerElement = (key, answerText) => {
   element.innerHTML = String.raw`
     ${key}: ${answerText};
   `;
+  element.addEventListener('click', () => {
+    const { currentQuestionIndex, questions } = quizData;
+    questions[currentQuestionIndex].selected = key;
+  });
+
   return element;
 };
